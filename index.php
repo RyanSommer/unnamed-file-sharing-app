2c816ef0f3a2bfdaf0bdb20282d484efe2d9d4b0
Ryan Sommer <ryan@rsommer.me>
2017-11-23 08:30:05 -0500

    Created basic app structure
    Just setting up.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>File Sharing</title>
+</head>
+<body>
+
+	<h1>File Sharing App</h1>
+
+	<!-- Scripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="/scripts/global.js"></script>
+	
+</body>
+</html>
